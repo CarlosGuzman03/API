4af441bbf2811930643cc2b21e70e04d82219ed7
Carlos Guzmán <carlosguzman0316@gmail.com>
2025-02-07 08:51:50 -0600

    Thunder Client

--- Parcial 1/ServerExpress/index.js
+++ Parcial 1/ServerExpress/index.js
@@ -16,11 +16,39 @@ next();
 app.use(express.json());
 app.use(express.text());
 
-app.get('/', (req,res,next)=>{
+
+app.get('/alumnos', (req,res,next)=>{
+    console.log(req.query)
     res.sendFile(__dirname+'/index.html');
 })
 
-app.post('/', (req,res)=>{
+app.patch ('/maestros', (req,res)=>{
+    console.log(req.params)
+    res.send("hello");
+})
+
+
+
+app.listen(port,()=>{
+    console.log('Server running at http://localhost:3000');
+});
+
+//.query .params .body
+
+//Cadena de Consulta - Correcto
+app.get('/alumnos', (req,res,next)=>{
+    console.log(req.query)
+    res.sendFile(__dirname+'/public/index.html');
+})
+
+//Parte de la Ruta
+app.patch ('/Sistemas/:control', (req,res)=>{
+    console.log(req.params)
+    res.sendFile(__dirname+'/public/index.html');
+})
+
+//Body (Json/Text)
+app.patch ('/maestros', (req,res)=>{
     console.log(req.body)
     res.send("hello");
 })
@@ -29,7 +57,3 @@ app.use('/', (req,res,next)=>{
     res.status(404);
     res.send("Error 404");
 })
-
-app.listen(port,()=>{
-    console.log('Server running at http://localhost:3000');
-});
